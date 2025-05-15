b26ad2df610a1cb48d29f5fb4f8e20eda925b755
Lucas Sabbag Muto <lucas.muto@RCNITEROI.IMOB>
2025-05-14 22:46:02 -0300

    objeto colocado como teste

---
+++ meu-modelo-3d/src/components/Garrafa.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { useGLTF } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
+import { Mesh } from 'three'
+
+export default function Garrafa() {
+  const gltf = useGLTF('/models/garrafav1.gltf')
+  const ref = useRef<Mesh>(null)
+
+  useFrame(() => {
+    if (ref.current) {
+      ref.current.rotation.y += 0.01
+    }
+  })
+
+  return <primitive object={gltf.scene} ref={ref} scale={1.5} />
+}
